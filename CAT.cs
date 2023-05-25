73585c6b56c12d6d006b702f1cc0ebda253f0fb0
Deiju <ilushenka2005zh@gmail.com>
2023-05-26 00:00:07 +0500

    321

---
+++ CAT.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_с_классами_Консоль_
+{
+    class CAT
+    {
+        public CAT (string CatName, double weight)
+        {
+            Name=CatName;
+            Weight = weight;
+        }
+        public void Meow ()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!! И его вес:{weight}");
+        }
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                bool OnlyLetters = true;
+                foreach ( var ch in value )
+                {
+                    if ( !char.IsLetter(ch) )
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if ( OnlyLetters )
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        private double weight;
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {   
+                if (!(value<=0))
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value}: не может быть нулевой или отрицательный вес");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_с_классами_Консоль_
+{
+    class Program
+    {
+        static void Main (string [ ] args)
+        {
+            CAT murzik = new CAT("Мурзик",13);
+            CAT barsik = new CAT("Барсик",20);
+            barsik.Weight = 0;
+            murzik.Weight = -65;
+            barsik.Name = "Барс1";
+            murzik.Name = "Мурз2";
+            murzik.Meow( );
+            barsik.Meow( );
+            Console.ReadLine( );
+        }
+    }
+}
